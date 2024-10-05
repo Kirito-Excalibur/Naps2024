b882026695fe242b2ac26632cdb721c528462794
Nikhil Simon Toppo <nikhilsimontoppo@gmail.com>
2024-10-05 19:56:39 +0530

    Editorial Section added

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { Link } from "@remix-run/react";
+import { Link,Form } from "@remix-run/react";
 import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
 import { useOptionalUser } from "./utils";
@@ -36,16 +36,33 @@ export default function App() {
       </head>
       <body className="h-full">
         <header className="flex items-center  bg-[rgb(177,4,14)] p-4 gap-5 text-white">
+        <Link to="/">Home</Link>
           <Link to="/editorials">Editorials</Link>
 
           { user ? (
+            <>
             <p>Logout</p>
+            <Link to="/notes">My Notes</Link>
+            
+            <Form action="/logout" method="post">
+          <button
+            type="submit"
+            className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+          >
+            Logout
+          </button>
+        </Form>
+
+            </>
           ) : (
-            <p>
+            <>
               <Link to="/join">Sign Up</Link>
               <Link to="/login">Log In </Link>
-            </p>
+            </>
           )}
+
+          <img src="napslogo.webp" width={"250px"} alt="" />
+
         </header>
         <Outlet />
         <ScrollRestoration />
--- app/routes/editorials.tsx
+++ app/routes/editorials.tsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import React from "react";
-import { json,Link } from "@remix-run/react";
+import { json } from "@remix-run/react";
 
 import { getAllNotes } from "~/models/note.server";
 
@@ -9,23 +9,26 @@ export const loader = async () => {
   return json({ noteListItems });
 };
 
-const editorials = () => {
+const Editorials = () => {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div>
-
-      <h1>All Notes</h1>
-      <div className="flex gap-4 ">
+    <>
+      <h1 className="text-2xl text-center my-4">All Notes</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {data.noteListItems.map((note) => (
-          <div className="border border-black" key={note.id}>
-            <h2>{note.title}</h2>
-            <h2>{note.user.email}</h2>
+          <div
+            className="border border-black p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+            key={note.id}
+          >
+            <img src="thumb.jpeg" alt="Thumnail Here" />
+            <h2 className="font-bold text-lg mb-2">{note.title}</h2>
+            <p className="text-sm text-gray-500">{note.user.email}</p>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
-export default editorials;
+export default Editorials;
