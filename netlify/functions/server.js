c521f578954ba5817330800e86704991df1e204c
Nikhil Simon Toppo <nikhilsimontoppo@gmail.com>
2024-10-09 21:13:10 +0530

    Added netlify.toml re 7

---
+++ netlify/functions/server.js
@@ -0,0 +1,10 @@
+
+const { createRequestHandler } = require('@remix-run/netlify');
+
+// Correctly reference the build/server.js file
+const build = require("../build")
+
+exports.handler = createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV || 'development',
+});
