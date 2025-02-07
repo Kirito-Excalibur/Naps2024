d1b6612d05a3a085b27b5840dce5d39a74a54032
Nikhil Simon Toppo <nikhilsimontoppo@gmail.com>
2025-02-07 20:09:31 +0530

    Switch from email to name

--- app/routes/editorials.$noteId.tsx
+++ app/routes/editorials.$noteId.tsx
@@ -43,7 +43,7 @@ export default function NoteDetailsPage() {
 
       <h3 className="font-tinos">
         
-        Author name:- {data.note.user.email}
+        Author name:- {data.note.user.name}
       </h3>
       <p
         className="font-tinos py-6 flex justify-center break-word align-center items-center flex-col text-base md:text-lg"
